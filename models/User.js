c9c22be4939c33967dd721a8a9eadb265fa8908a
Geethapriya2815 <s.l.geethapriya8888@gmail.com>
2025-10-30 10:49:36 +0530

    update my file

--- models/User.js
+++ models/User.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-
 const userSchema = new mongoose.Schema({
     name: {
         type: String, required: true 
@@ -11,5 +10,4 @@ const userSchema = new mongoose.Schema({
         type: Number, required: true, min: 0
     },
 });
-
 module.exports = mongoose.model("User", userSchema);
